eede32a4ba3fcc0735ef45cca67ca1104e834715
crazycodeboy <crazycodeboy@gmail.com>
2015-12-15 17:45:01 +0800

    init

---
+++ Ios-HomeUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ios-HomeUI
+//
+//  Created by OEFFECT on 15/12/15.
+//  Copyright © 2015年 jph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ios-HomeUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ios-HomeUI
+//
+//  Created by OEFFECT on 15/12/15.
+//  Copyright © 2015年 jph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
